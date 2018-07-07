a2c1cb720f6309e2f764e63c1a0854cc2232a153
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-07 15:04:33 +0300

    added first query

---
+++ Lesson2_ASP.NET/Controllers/QueryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lesson2_ASP.NET.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson2_ASP.NET.Controllers
+{
+    public class QueryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /query/commentnumberbyuserpost/
+        public IActionResult CommentNumberByUserPosts(int id)
+        {
+            var query = UserService.Instance.GetCommentNumberByUserPosts(id)?.ToList();
+
+            if(query == null || query.Count == 0)
+            {
+                ViewData["Success"] = "Don`t have any data";
+            }
+
+            return View(query);
+        }
+    }
+}
