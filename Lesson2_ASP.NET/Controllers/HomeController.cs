e4b14e310a175d8579bce875dcb836dbb6702f47
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-08 00:05:38 +0300

    created users feed

--- Lesson2_ASP.NET/Controllers/HomeController.cs
+++ Lesson2_ASP.NET/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Lesson2_ASP.NET.Models;
+using Lesson2_ASP.NET.Services;
+
 
 namespace Lesson2_ASP.NET.Controllers
 {
@@ -12,7 +14,7 @@ namespace Lesson2_ASP.NET.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(UserService.Instance.Users);
         }
 
         public IActionResult Error()
--- Lesson2_ASP.NET/Services/UserService.cs
+++ Lesson2_ASP.NET/Services/UserService.cs
@@ -11,18 +11,18 @@ namespace Lesson2_ASP.NET.Services
 
         public static UserService Instance { get { return lazy.Value; } }
 
-        private readonly List<User> users;
+        public List<User> Users { get; private set; }
 
         private UserService()
         {
-            users = DBService.Users;
+            Users = DBService.Users;
         }
 
-        public User GetUser(int id) => users.FirstOrDefault(u => u.Id.Equals(id));
+        public User GetUser(int id) => Users.FirstOrDefault(u => u.Id.Equals(id));
 
         public Post GetPost(int id)
         {
-            var post = (from u in users
+            var post = (from u in Users
                         from p in u.Posts
                         where p.Id == id
                         select p).FirstOrDefault();
@@ -32,7 +32,7 @@ namespace Lesson2_ASP.NET.Services
 
         public Todo GetTodo(int id)
         {
-            var todo = (from u in users
+            var todo = (from u in Users
                         from t in u.Todos
                         where t.Id == id
                         select t).FirstOrDefault();
@@ -43,7 +43,7 @@ namespace Lesson2_ASP.NET.Services
 
         public IEnumerable<(Post Post, int CommentNumber)> GetCommentNumberByUserPosts(int userId)
         {
-            return users.FirstOrDefault(u => u.Id == userId)?.Posts.Select(p => (p, p.Comments.Count));
+            return Users.FirstOrDefault(u => u.Id == userId)?.Posts.Select(p => (p, p.Comments.Count));
         }
 
         public IEnumerable<Comment> GetCommentsByUserPosts(int userId)
@@ -65,17 +65,17 @@ namespace Lesson2_ASP.NET.Services
 
         public IEnumerable<(int Id, string Name)> GetFineshedTodosByUser(int userId)
         {
-            return users.FirstOrDefault(u => u.Id == userId)?.Todos.Where(t => t.IsComplete).Select(t => (t.Id, t.Name));
+            return Users.FirstOrDefault(u => u.Id == userId)?.Todos.Where(t => t.IsComplete).Select(t => (t.Id, t.Name));
         }
 
         public IEnumerable<User> GetSortUsersAndTodos()
         {
-            return users.OrderBy(u => u.Name).ThenByDescending(u => u.Todos.Select(t => t.Name.Length));
+            return Users.OrderBy(u => u.Name).ThenByDescending(u => u.Todos.Select(t => t.Name.Length));
         }
 
         public UserInfo GetUserInfo(int userId)
         {
-            var user = users.FirstOrDefault(u => u.Id == userId);
+            var user = Users.FirstOrDefault(u => u.Id == userId);
             var lastPost = user?.Posts.OrderByDescending(p => p.CreatedAt).FirstOrDefault();
             var commentNumberByLastPost = lastPost?.Comments.Count;
             var unfinishedTodoNumber = user?.Todos.Where(t => !t.IsComplete).Count();
